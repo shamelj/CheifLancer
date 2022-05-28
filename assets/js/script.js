e6c77064c72c02321dd2a62160fa70f5a7204cef
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2022-05-28 14:29:51 +0300

    posts function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,48 @@
-addMeal = (cookName,mealname,description,price,waitingTime,arrayOfPics) => {
- 
+let cook ='abu zant';
+let me='la7em 7alal';
+let de='very good lahem yes very nice yyyeees yeeees';
+let price ='55$';
+let waitingT='16 days';
+let arrImage =['https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg?quality=82&strip=1&w=640','https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg?quality=82&strip=1&w=640'];
+addMeal = (cookName,mealname,mprice,description,waitingT,arrImg) => {
+    
     const meal = document.createElement("div");
-    const div = document.createElement("div");
-    const Name = document.createElement("p");
+    const pic = document.createElement("div");
+    const pic1 = document.createElement("div");
+    const pic2 = document.createElement("div");
+    const details = document.createElement("div");
+    const name = document.createElement("p");
+    const price = document.createElement("p");
     const mealn = document.createElement("p");
     const desc = document.createElement("p");
+    const waitingTime = document.createElement("p");
+    details.className="details";
     meal.className = "meal";
-    Name.textContent = cookName;
+    pic.className="pic";
+    pic1.className="pic1";
+    pic2.className="pic2";
+    pic1.style.backgroundImage="url('"+arrImg[0]+"')";
+    if(arrImg[1]!=null){
+    pic.appendChild(pic2);
+     pic2.style.backgroundImage="url('"+arrImg[1]+"')";
+    }
+    name.textContent = cookName;
+    price.textContent=mprice;
     mealn.textContent = mealname;
-   desc .textContent = description;
+    desc.textContent  = description;
+    waitingTime.textContent =waitingT;
 
-    
-    meal.appendChild(div);
-    meal.appendChild(mealn);
-    meal.appendChild(Name);
+    pic.appendChild(pic1);
+    meal.appendChild(pic);
+    details.appendChild(mealn);
+    details.appendChild(desc);
+    details.appendChild(price);
+    details.appendChild(name);
+    details.appendChild(waitingTime);
 
-
-
+    meal.appendChild(details);
     document.querySelector("#meals").appendChild(meal);
 }
+addMeal(cook,me,price,de,waitingT,arrImage);
+
+
