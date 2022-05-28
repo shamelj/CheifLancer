33cb5de4c7dc2611287cf41402132490d110d6f5
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2022-05-28 16:45:17 +0300

    responsive cust

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let me='la7em 7alal';
 let de='very good lahem yes very nice yyyeees yeeees';
 let price ='55$';
 let waitingT='16 days';
-let arrImage =['https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg?quality=82&strip=1&w=640','https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg?quality=82&strip=1&w=640'];
+let arrImage =['https://img.freepik.com/free-photo/big-hamburger-with-double-beef-french-fries_252907-8.jpg?w=2000','https://images.everydayhealth.com/images/diet-nutrition/34da4c4e-82c3-47d7-953d-121945eada1e00-giveitup-unhealthyfood.jpg?sfvrsn=a31d8d32_0'];
 addMeal = (cookName,mealname,mprice,description,waitingT,arrImg) => {
     
     const meal = document.createElement("div");
@@ -21,25 +21,34 @@ addMeal = (cookName,mealname,mprice,description,waitingT,arrImg) => {
     pic.className="pic";
     pic1.className="pic1";
     pic2.className="pic2";
+    name.className="name";
+    price.className="price";
+    mealn.className="mealn";
+
+    mealn.style.fontSize ="22";
+    mealn.style.fontWeight="bold";
+    price.style.fontSize="45"
+    
     pic1.style.backgroundImage="url('"+arrImg[0]+"')";
+    pic1.style.backgroundRepeat='no-repeat'
     if(arrImg[1]!=null){
-    pic.appendChild(pic2);
-     pic2.style.backgroundImage="url('"+arrImg[1]+"')";
+     pic2.style.background="url('"+arrImg[1]+"')";
+     pic2.style.backgroundRepeat='no-repeat';
+     pic.appendChild(pic2);
     }
-    name.textContent = cookName;
+    name.textContent = "Cheif :" + cookName;
     price.textContent=mprice;
     mealn.textContent = mealname;
     desc.textContent  = description;
-    waitingTime.textContent =waitingT;
+    waitingTime.textContent ="order is deliverd in less than"+ waitingT;
 
     pic.appendChild(pic1);
-    meal.appendChild(pic);
     details.appendChild(mealn);
     details.appendChild(desc);
     details.appendChild(price);
     details.appendChild(name);
     details.appendChild(waitingTime);
-
+    meal.appendChild(pic);
     meal.appendChild(details);
     document.querySelector("#meals").appendChild(meal);
 }
